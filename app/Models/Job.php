b6636885e38fb593269207a52ca924cf5424a3f8
Lené <126817925+Lene7@users.noreply.github.com>
2025-01-15 20:59:57 +0200

    Ep15-19

--- app/Models/Job.php
+++ app/Models/Job.php
@@ -1,4 +1,4 @@
-<?php 
+<?php
 
 namespace App\Models;
 
@@ -7,10 +7,10 @@ use Illuminate\Database\Eloquent\Factories\HasFactory;
 
 class Job extends Model{
     use HasFactory;
-    
+
     protected $table = 'job_listings';
 
-    protected $fillable = ['title', 'salary'];
+    protected $guarded = [];
 
     public function employer()
     {
