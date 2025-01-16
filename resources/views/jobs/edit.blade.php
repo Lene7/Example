293272c0c17626790fff7787a065c257d19412f9
Lené <126817925+Lene7@users.noreply.github.com>
2025-01-16 15:45:10 +0200

    Ep 19-23

--- resources/views/jobs/edit.blade.php
+++ resources/views/jobs/edit.blade.php
@@ -60,7 +60,7 @@
 
             <div class="flex items-center gap-x-6">
                 <a href="/jobs/{{ $job->id }}" class="text-sm/6 font-semibold text-gray-900">Cancel</a>
-                <button type="submit" class="rounded-md bg-indigo-600 px-3 py-2 text-sm font-semibold text-white shadow-sm hover:bg-indigo-500 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600">Update</button>
+                <x-form-button>Update</x-form-button>
             </div>
     </form>
 
--- resources/views/jobs/show.blade.php
+++ resources/views/jobs/show.blade.php
@@ -11,7 +11,9 @@
         This job pays {{ $job->salary }} per month.
     </p>
 
-    <p class="mt-6">
-        <x-button href="/jobs/{{ $job->id }}/edit">Edit Job</x-button>
-    </p>
+    @can('edit-job', $job)
+        <p class="mt-6">
+            <x-button href="/jobs/{{ $job->id }}/edit">Edit Job</x-button>
+        </p>
+    @endcan
 </x-layout>
